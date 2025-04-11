d58b0ef0a883866cc323542bb10f3b8a50874f2f
Felipe S. Freitas <133303018+FelipeSF13@users.noreply.github.com>
2025-04-11 19:57:56 -0300

    Add files via upload

--- Script.sql
+++ Script.sql
@@ -7,26 +7,27 @@ USE ESTOQUE_ECOMM;
 
 -- Criação da tabela "produtos"
 CREATE TABLE produtos (
-    ID INT IDENTITY(1,1) PRIMARY KEY,         -- Identificador único do produto, autoincrementado
-    NOME VARCHAR(255) NOT NULL,                -- Nome do produto
-    DESCRICAO TEXT,                            -- Descrição do produto
-    PRECO DECIMAL(10, 2) NOT NULL,             -- Preço do produto (com duas casas decimais)
-    QUANTIDADE_EM_ESTOQUE INT NOT NULL         -- Quantidade em estoque
+    ID INT PRIMARY KEY,
+    NOME VARCHAR(100),
+    CATEGORIA VARCHAR(50),
+    PRECO DECIMAL(10, 2),
+    DESCRICAO TEXT,
+    QUANTIDADE_EM_ESTOQUE INT
 );
 
 -- Inserção de dados exemplo na tabela "produtos"
-INSERT INTO produtos (NOME, DESCRICAO, PRECO, QUANTIDADE_EM_ESTOQUE)
+INSERT INTO Produtos (ID, NOME, CATEGORIA, PRECO, DESCRICAO, QUANTIDADE_EM_ESTOQUE) 
 VALUES
-    ('Relógio Rolex', 'Relógio dourado', 1999.99, 50),
-    ('Bracelete de Couro', 'Bracelete de couro vermelho trancado', 3499.99, 30),
-    ('Bracelete de prata', 'Bracelete de prata ajustável Rivera brilhante', 499.99, 100),
-    ('Relógio de luxo', 'Relógio de ouro 18K', 12999.99, 10),
-    ('Pulseira de prata', 'Pulseira em prata 925', 899.99, 25),
-    ('Colar de diamantes', 'Colar cravejado em diamantes', 25499.99, 5),
-    ('Anel de safira', 'Anel com safira azul', 7999.99, 15),
-    ('Brincos de ouro', 'Brincos em ouro 18K', 3499.99, 20),
-    ('Relógio smart', 'Relógio inteligente com GPS', 2199.99, 40),
-    ('Pingente de esmeralda', 'Pingente com esmeralda colombiana', 6999.99, 12);
+(1, 'Relógio Rolex', 'Relógio', 1999.90, 'Relógio dourado', 10),
+(2, 'Corrente de Prata', 'Corrente', 900.00, 'Corrente 60 cm', 10),
+(3, 'Pulseira de Prata', 'Pulseira', 600.00, 'Pulseira trançada', 10),
+(4, 'Brinco de ouro', 'Brinco', 250.00, 'Par dourado', 10),
+(5, 'Relógio de Luxo', 'Relógio', 12000.00, 'Patek Philippe', 10),
+(6, 'Bracelete de Prata', 'Pulseira', 499.90, 'Berloque', 10),
+(7, 'Anel de Safira', 'Anel', 7999.90, 'Anel madreperola', 10);
+
+
+
 
 
 
@@ -49,8 +50,12 @@ VALUES
     ('wesley.mssantos3@senacsp.edu.br', 'senha123'),
     ('farthurdiasdasilva@gmail.com', 'senha456'),
     ('Claudio.fjunior20@outlook.com', 'senha789'),
+	('felipe.souza.freitasfs@gmail.com', 'senha567'),
     ('juliae_os@outlook.com.br', 'senha1011');
 
 
 
-	SELECT * FROM usuarios;
+
+SELECT * FROM produtos
+
+
